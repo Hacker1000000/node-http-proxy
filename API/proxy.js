ffbc7aa3ccf9382a88b28a51a5d771e898973db4
Hacker1000000000 <95496750+Hacker1000000@users.noreply.github.com>
2025-09-17 08:25:21 -0500

    Delete API directory

--- API/proxy.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
-
-const proxy = httpProxy.createProxyServer({});
-
-module.exports = (req, res) => {
-  proxy.web(req, res, { target: 'https://example.com' }); // replace with target URL
-};
